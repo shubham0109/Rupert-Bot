28ae0606ec40eda3ae329ddd387282e02a0591b2
Daniel Shiffman <daniel@shiffman.net>
2016-11-23 15:03:55 -0500

    bot code

---
+++ bot/bot.js
@@ -0,0 +1,143 @@
+// A2Z F16
+// Daniel Shiffman
+// http://shiffman.net/a2z
+// https://github.com/shiffman/A2Z-F16
+
+var livetweets = [
+  'It\'s almost time for tonight\'s episode of #Survivor. Will you be around to live tweet with me? #survivor',
+  'Each Wednesday night I live tweet Survivor! Watch along with me!'
+];
+
+// Using the Twit node package
+// https://github.com/ttezel/twit
+var Twit = require('twit');
+var rita = require('rita');
+
+// Pulling all my twitter account info from another file
+var config = require('./config.js');
+
+// Making a Twit object for connection to the API
+var T = new Twit(config);
+
+
+var fs = require('fs');
+
+var txt = fs.readFileSync('rupert.txt', 'utf-8');
+
+var Concordance = require('./concordance.js');
+var MarkovGenerator = require('./markov.js');
+
+var wordcounts = new Concordance();
+wordcounts.process(txt);
+var keys = wordcounts.getKeys();
+
+var rm = new rita.RiMarkov(2);
+rm.loadText(txt);
+
+var markov = new MarkovGenerator(5, 140);
+var lines = txt.split('\n');
+for (var i = 0; i < lines.length; i++) {
+  markov.feed(lines[i]);
+}
+
+var CFG = require('./cfg');
+var cfg = new CFG();
+cfg.initGrammar();
+cfg.addWords(wordcounts);
+
+// Start once
+tweeter();
+
+// Once every N milliseconds
+setInterval(tweeter, 60 * 5 * 1000);
+
+// Here is the bot!
+function tweeter() {
+  var live = true;
+  var starting = false;
+  var d = new Date();
+  var day = d.getUTCDay();
+  var hours = d.getUTCHours();
+  var minutes = d.getUTCMinutes();
+
+  // Only tweet UTC Thursdays 1am-5am
+  // Announce tweeting somwhere in the 5 minutes until range
+  console.log(day,hours,minutes);
+  if (day == 4 && hours == 0 && minutes > 54) {
+    starting = true;
+  }
+  if (day != 4) {
+    live = false;
+  }
+  if (hours < 1 || hours > 6) {
+    live = false;
+  }
+
+  var tweet;
+  if (starting) {
+    var index = Math.floor(Math.random() * livetweets.length)
+    tweet = livetweets[index];
+  } else if (live) {
+    tweet = generateTweet();
+  }
+
+  if (starting || live) {
+    T.post('statuses/update', {
+      status: tweet
+    }, tweeted);
+  }
+
+  // Callback for when the tweet is sent
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Success: ' + data.text);
+      //console.log(response);
+    }
+  };
+
+}
+
+function generateTweet() {
+  var tweet;
+
+  var r = Math.random();
+  if (r < 0.33) {
+    console.log('char markov');
+    tweet = markov.generate();
+  } else if (r < 0.66) {
+    console.log('sentence markov');
+    var result = rm.generateSentences(1);
+    tweet = result[0];
+    //tweet = result[0] + ' ' + rg.expandFrom('<HASHTAG>');
+  } else {
+    console.log('cfg');
+    var result = cfg.expand();
+    var output = [];
+    var sentences = result.split(/([.?!]\s+)/);
+    for (var i = 0; i < sentences.length; i++) {
+      var words = sentences[i].split(/(\s+)/);
+      for (var j = 0; j < words.length; j++) {
+        var word = words[j];
+        if (j == 0) {
+          var c = word.charAt(0);
+          word = c.toUpperCase() + word.substring(1, word.length);
+        } else {
+          var pos = rita.RiTa.getPosTags(word);
+          if (word.length > 1 && !/^[A-Z]+$/.test(word) && !/nnp/.test(pos[0])) {
+            word = word.toLowerCase();
+          }
+        }
+        output.push(word);
+      }
+    }
+    tweet = output.join('');
+  }
+
+  if (tweet.length > 140) {
+    tweet = tweet.substring(0, 140);
+  }
+  tweet = tweet.replace(/@/, '');
+  return tweet;
+}
---
+++ bot/cfg.js
@@ -0,0 +1,80 @@
+module.exports = CFG;
+
+var rita = require('rita');
+
+function CFG() {
+  this.rg = new rita.RiGrammar();
+}
+
+CFG.prototype.initGrammar = function() {
+  this.rg.addRule('<start>', '<S> <HASHTAG>');
+  this.rg.addRule('<start>', '<S> <HASHTAG> <HASHTAG>');
+  this.rg.addRule('<start>', '<S> <S> <HASHTAG>');
+  this.rg.addRule('<NVP>', '<NP> <VP>')
+  this.rg.addRule('<NVP>', '<NP> <MD> <VP>')
+  this.rg.addRule('<S>', '<UH><PUNC> <S>');
+  this.rg.addRule('<S>', '<NVP><PUNC>');
+  this.rg.addRule('<S>', '<NVP> <CC> <S>');
+  this.rg.addRule('<NP>', '<DT> <JJ> <NN>');
+  this.rg.addRule('<NP>', '<DT> <NN>');
+  this.rg.addRule('<NOUN>', '<NN>');
+  this.rg.addRule('<NOUN>', '<NNS>');
+  this.rg.addRule('<NP>', '<DT> <NOUN> <CC> <NOUN>', 0.1);
+  this.rg.addRule('<NP>', '<NOUN>');
+  this.rg.addRule('<NP>', '<CD> <NOUN>');
+  this.rg.addRule('<NP>', '<NOUN> <CC> <NOUN>', 0.1);
+  this.rg.addRule('<PPN>', '<NNP>');
+  this.rg.addRule('<PPN>', '<NNPS>');
+  this.rg.addRule('<NP>', '<PPN>');
+  this.rg.addRule('<NP>', '<PPN> <CC> <PRP>', 0.1);
+  this.rg.addRule('<NP>', '<PRP>');
+  this.rg.addRule('<VERB>', '<VB>');
+  this.rg.addRule('<VERB>', '<VBD>');
+  this.rg.addRule('<VP>', '<VERB>');
+  this.rg.addRule('<VP>', '<VERB> <RB>');
+  this.rg.addRule('<VP>', '<VERB> <NP>');
+  this.rg.addRule('<VP>', '<VERB> <NP> <PP>');
+  this.rg.addRule('<VP>', '<VERB> <PP>');
+  this.rg.addRule('<PP>', '<IN> <NP>');
+  this.rg.addRule('<ADJP>', '<JJ>');
+  this.rg.addRule('<ADJP>', '<JJR>');
+  this.rg.addRule('<ADJP>', '<PRP$>');
+  this.rg.addRule('<ADJP>', '<JJS>');
+  this.rg.addRule('<PUNC>', '.');
+  this.rg.addRule('<PUNC>', '?');
+  this.rg.addRule('<PUNC>', '!');
+}
+
+// MISSING pos
+// http://rednoise.org/rita/reference/PennTags.html
+// EX WRB WP WDT VBZ VBP
+
+CFG.prototype.addWords = function(wordcounts) {
+  var words = wordcounts.getKeys();
+  // Go through all the words
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    var count = wordcounts.getCount(word);
+    var prob = count / wordcounts.total;
+    //console.log(word, prob);
+
+    if (word.charAt(0) == '#') {
+      this.rg.addRule('<HASHTAG>', word, prob);
+    } else if (word.charAt(0) == '@') {
+      this.rg.addRule('NNP', word)
+    } else {
+      // Use RiTa's Analyzer to determine syllable count
+      var pos = rita.RiTa.getPosTags(word);
+      for (var j = 0; j < pos.length; j++) {
+        var p = '<' + pos[j].toUpperCase() + '>';
+        this.rg.addRule(p, word)
+      }
+    }
+    // Syllables are separated with colons
+  }
+
+}
+
+CFG.prototype.expand = function() {
+  return this.rg.expand();
+}
