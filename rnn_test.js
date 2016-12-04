fd3a5b709d3183c7eed365d365752ea321d0bff3
Daniel Shiffman <daniel@shiffman.net>
2016-12-04 09:36:59 -0500

    fixing replies

--- rnn_test.js
+++ rnn_test.js
@@ -3,11 +3,11 @@
 
 var spawn = require('child_process').spawn;
 
-var params = ['sample.lua', 'rnn/lm_lstm_epoch50.00_1.5083.t7_cpu.t7', '-length', '100'];
+var params = ['sample.lua', 'rnn/lm_lstm_epoch50.00_1.5083.t7_cpu.t7', '-length', '140'];
 params[4] = '-temperature';
 params[5] = Math.random()*0.9 + 0.1;
 params[6] = '-primetext';
-params[7] = 'Reward challenge';
+params[7] = 'flawed 🙉';
 params[8] = '-seed';
 params[9] = Math.floor(Math.random()*1000);
 //console.log(params[5]);
